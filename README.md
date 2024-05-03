6c1dbcf6e47a9507c764a83dff8a66a150b725b1
hewong5 <hwong6785@gmail.com>
2024-05-02 20:14:02 -0700

    implemented drawing board and shuffling cards; added background color

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ECS162-Game-Arcade
 
 ## Image Credits
+### Dice Game
 - [dice1.png](https://en.m.wikipedia.org/wiki/File:Dice-1-b.svg)
 - [dice2.png](https://en.m.wikipedia.org/wiki/File:Dice-2-b.svg)
 - [dice3.png](https://en.m.wikipedia.org/wiki/File:Dice-3-b.svg)
@@ -8,6 +9,21 @@
 - [dice5.png](https://en.m.wikipedia.org/wiki/File:Dice-5-b.svg)
 - [dice6.png](https://en.m.wikipedia.org/wiki/File:Dice-6-b.svg)
 - [dice-roll.jpg](https://images.app.goo.gl/axDTYxGUmatxJLv39)
+
+### Memory Game
+- [blastoise.png](https://images.app.goo.gl/J1TR7xQ4qfQFQhMs7)
+- [bulbasaur.png](https://images.app.goo.gl/sHHGSjJ9jNkgjTdk9)
+- [card-back.png](https://images.app.goo.gl/SWGx3zMUAjGvgx3T6)
+- [charizard.png](https://images.app.goo.gl/WUUXMYY7dNrhH6Tg8)
+- [charmander.png](https://images.app.goo.gl/6b3w92i7q3SZwUiy6)
+- [eevee.png](https://images.app.goo.gl/gazZmPvzcFxpr9Lt6)
 - [memory-game.jpg](https://images.app.goo.gl/iTqWm7sxGJKSezKF6)
+- [meowth.png](https://images.app.goo.gl/kUnXze5TeCjonKts8)
+- [pikachu.png](https://images.app.goo.gl/pWGXVCoY3fJEbsYd9)
+- [squirtle.png](https://images.app.goo.gl/CZwkJwaxCiSfxwZC6)
+- [venusaur.png](https://images.app.goo.gl/JYf7BMjDzrCbM4Ch9)
+- [vulpix.png](https://images.app.goo.gl/mHLvAJN4WBSA7iVLA)
+
+### Wordle
 - [wordle.jpg](https://images.app.goo.gl/izQypukypuzjjMpZ6)
 - [wordleList](https://gist.github.com/slushman/34e60d6bc479ac8fc698df8c226e4264)
---
+++ memoryGame/memoryGame.js
@@ -0,0 +1,61 @@
+let boardState = [];
+let cardsList = [
+    'blastoise.png',
+    'bulbasaur.png',
+    'charizard.png',
+    'charmander.png',
+    'eevee.png',
+    'meowth.png',
+    'pikachu.png',
+    'squirtle.png',
+    'venusaur.png',
+    'vulpix.png'
+];
+
+let cardsShuffled;
+let numRows = 5;
+let numCols = 4;
+
+function shuffleCards() {
+    cardsShuffled = cardsList.concat(cardsList);
+    for(let i = 0; i < cardsShuffled.length; i++) {
+        let randomIndex = Math.floor(Math.random() * cardsShuffled.length);
+        let temp = cardsShuffled[i];
+        cardsShuffled[i] = cardsShuffled[randomIndex];
+        cardsShuffled[randomIndex] = temp;
+    }
+}
+
+function drawCell(board, row, col) {
+    let cell = document.createElement('img');
+    cell.className = 'cell';
+    cell.id = `cell${row}${col}`;
+    cell.src = cardsShuffled.pop();
+    board.appendChild(cell);
+    return cell.src;
+}
+
+function drawBoard(game) {
+    let board = document.createElement('div');
+    board.classList.add('board');
+    for(let i = 0; i < numRows; i++) {
+        boardState.push([]);
+        for(let j = 0; j < numCols; j++) {
+            let img = drawCell(board, i, j);
+            boardState[i].push(img);
+        }
+    }
+    game.appendChild(board);
+}
+
+function initializeBoard() {
+    let game = document.getElementById('game-container');
+    drawBoard(game);
+}
+
+function startGame() {
+    shuffleCards();
+    initializeBoard();
+}
+
+startGame()
